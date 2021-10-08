685a3ee3061a5aeb013f66150680626b1edb02e6
Tony Nguyen <31168287+VeryHotShark@users.noreply.github.com>
2021-10-08 15:37:24 +0200

    Initial commit

---
+++ Assets/Scripts/ClientHandle.cs
@@ -0,0 +1,25 @@
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientHandle : MonoBehaviour
+{
+    public static void Welcome(Packet packet) {
+        string msg = packet.ReadString();
+        int myId = packet.ReadInt();
+
+        Debug.Log($"Message from server: {msg}");
+        Client.instance.myId = myId;
+        ClientSend.WelcomeReceived();
+
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+
+    public static void UDPTest(Packet packet) {
+        string msg = packet.ReadString();
+
+        Debug.Log($"Received packet via UDP. Contains message: {msg}");
+        ClientSend.UDPTestReceived();
+    }
+}
---
+++ Assets/Scripts/ClientSend.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSend : MonoBehaviour
+{
+    private static void SendTCPData(Packet packet) {
+        packet.WriteLength();
+        Client.instance.tcp.SendData(packet);
+    }
+
+     private static void SendUDPData(Packet packet) {
+        packet.WriteLength();
+        Client.instance.udp.SendData(packet);
+    }
+
+    #region Packets
+    public static void WelcomeReceived() {
+        using(Packet packet = new Packet((int) ClientPackets.welcomeReceived)) {
+            packet.Write(Client.instance.myId);
+            packet.Write(UIManager.instance.usernameField.text);
+
+            SendTCPData(packet);
+        }
+    }
+
+    public static void UDPTestReceived() {
+        using(Packet packet = new Packet((int) ClientPackets.udpTestReceived)) {
+            packet.Write("Received a UDP packet");
+
+            SendUDPData(packet);
+        }
+    }
+    #endregion
+}
