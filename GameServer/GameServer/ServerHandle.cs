8f2c6b85ca1f40926ac9580349f3e6c21df8bc63
Tony Nguyen <nguyen.tu.tony2000@gmail.com>
2021-10-21 10:40:26 +0200

    GameServer

---
+++ GameServer/GameServer/ServerHandle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace GameServer
+{
+        class ServerHandle
+        {
+                public static void WelcomeReceived(int fromClient, Packet packet)
+                {
+                        int clientIdCheck = packet.ReadInt();
+                        string username = packet.ReadString();
+
+                        Console.WriteLine($"{Server.Clients[fromClient].tcp.socket.Client.RemoteEndPoint} connected succesfully and is now player {fromClient} aka {username}.");
+
+                        if(fromClient != clientIdCheck)
+                        {
+                                Console.WriteLine($"Player \" {username} \" (ID: {fromClient}) has assumed the wrong client ID ({clientIdCheck})! ");
+                        }
+                        Server.Clients[fromClient].SendIntoGame(username);
+                }
+
+                public static void PlayerMovement(int fromClient, Packet packet)
+                {
+                        bool[] inputs = new bool[packet.ReadInt()];
+
+                        for (int i = 0; i < inputs.Length; i++)
+                        {
+                                inputs[i] = packet.ReadBool();
+                        }
+
+                        Quaternion rotation = packet.ReadQuaternion();
+                        Server.Clients[fromClient].player.SetInput(inputs, rotation);
+                }
+        }
+}
---
+++ GameServer/GameServer/ServerSend.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer
+{
+        class ServerSend
+        {
+                private static void SendTCPData(int toClient, Packet packet)
+                {
+                        packet.WriteLength();
+                        Server.Clients[toClient].tcp.SendData(packet);
+                }
+
+                private static void SendUDPData(int toClient, Packet packet)
+                {
+                        packet.WriteLength();
+                        Server.Clients[toClient].udp.SendData(packet);
+                }
+
+                private static void SendTCPDataToAll( Packet packet)
+                {
+                        packet.WriteLength();
+
+                        for (int i = 1; i <= Server.MaxPlayers; i++)
+                        {
+                                Server.Clients[i].tcp.SendData(packet);
+                        }
+                }
+
+                private static void SendTCPDataToAll(int exceptClient, Packet packet)
+                {
+                        packet.WriteLength();
+
+                        for (int i = 1; i <= Server.MaxPlayers; i++)
+                        {
+                                if (i == exceptClient)
+                                        continue;
+
+                                Server.Clients[i].tcp.SendData(packet);
+                        }
+                }
+
+                private static void SendUDPDataToAll(Packet packet)
+                {
+                        packet.WriteLength();
+
+                        for (int i = 1; i <= Server.MaxPlayers; i++)
+                        {
+                                Server.Clients[i].udp.SendData(packet);
+                        }
+                }
+
+                private static void SendUDPDataToAll(int exceptClient, Packet packet)
+                {
+                        packet.WriteLength();
+
+                        for (int i = 1; i <= Server.MaxPlayers; i++)
+                        {
+                                if (i == exceptClient)
+                                        continue;
+
+                                Server.Clients[i].udp.SendData(packet);
+                        }
+                }
+
+                #region Packets
+                public static void Welcome(int toClient, string msg)
+                {
+                        using (Packet packet = new Packet((int)ServerPackets.welcome))
+                        {
+                                packet.Write(msg);
+                                packet.Write(toClient);
+
+                                SendTCPData(toClient, packet);
+
+                        }
+                }
+
+                public static void SpawnPlayer(int toClient, Player player)
+                {
+                        using(Packet packet = new Packet((int)ServerPackets.spawnPlayer))
+                        {
+                                packet.Write(player.id);
+                                packet.Write(player.username);
+                                packet.Write(player.position);
+                                packet.Write(player.rotation);
+
+                                SendTCPData(toClient, packet);
+                        }
+                }
+
+                public static void PlayerPosition(Player player)
+                {
+                        using (Packet packet = new Packet((int)ServerPackets.playerPosition))
+                        {
+                                packet.Write(player.id);
+                                packet.Write(player.position);
+
+                                SendUDPDataToAll( packet);
+                        }
+                }
+
+                public static void PlayerRotation(Player player)
+                {
+                        using (Packet packet = new Packet((int)ServerPackets.playerRotation))
+                        {
+                                packet.Write(player.id);
+                                packet.Write(player.rotation);
+
+                                SendUDPDataToAll(player.id, packet);
+                        }
+                }
+                #endregion
+        }
+}
